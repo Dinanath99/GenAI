e364e835b93fd366e4a6273eb0ab5e5a72635b92
Dinanath <dkmdkm26@gmail.com>
2025-06-20 20:48:10 +0545

    cosine similarity added

--- embeddings/dist/index.js
+++ embeddings/dist/index.js
@@ -12,17 +12,50 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.loadFruitJsonFile = loadFruitJsonFile;
 const dotenv_1 = __importDefault(require("dotenv"));
+const fs_1 = require("fs");
 const openai_1 = require("openai");
+const path_1 = require("path");
 dotenv_1.default.config();
 const openai = new openai_1.OpenAI();
-function generateEmbeddings() {
+const dotProduct = (vecA, vecB) => {
+    return vecA.reduce((sum, value, index) => sum + value * vecB[index], 0);
+};
+const cosineSimilarity = (vecA, vecB) => {
+    const dotProd = dotProduct(vecA, vecB);
+    const magnitudeA = Math.sqrt(dotProduct(vecA, vecB));
+    const magniTudeB = Math.sqrt(dotProduct(vecA, vecB));
+    return dotProd / (magnitudeA * magniTudeB);
+};
+function loadFruitJsonFile(fileName) {
+    const filePath = (0, path_1.join)(__dirname, fileName);
+    const rawData = (0, fs_1.readFileSync)(filePath, "utf-8");
+    return JSON.parse(rawData.toString());
+}
+// console.log(loadFruitJsonFile("fruits.json"));
+function generateEmbeddings(fruitDescription) {
     return __awaiter(this, void 0, void 0, function* () {
         const response = yield openai.embeddings.create({
             model: "text-embedding-3-small",
             input: "Hello, how are you ?",
         });
-        console.log(response);
+        return response;
     });
 }
-generateEmbeddings();
+const saveDataToJsonFile = (fileName, data) => {
+    const filePath = (0, path_1.join)(__dirname, fileName);
+    const jsonData = JSON.stringify(data, null, 2);
+    (0, fs_1.writeFileSync)(filePath, jsonData, "utf-8");
+};
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const fruits = loadFruitJsonFile("fruits.json");
+        const fruitDescription = fruits.map((fruit) => fruit.description);
+        // console.log(fruitDescription);
+        const embeddings = yield generateEmbeddings(fruitDescription);
+        const fruitsWithEmbeddings = fruits.map((fruit, index) => (Object.assign(Object.assign({}, fruit), { embedding: embeddings.data[index].embedding })));
+        saveDataToJsonFile("fruits_with_embeddings.json", fruitsWithEmbeddings);
+    });
+}
+run();
--- embeddings/src/index.ts
+++ embeddings/src/index.ts
@@ -1,13 +1,57 @@
 import dotenv from "dotenv";
+import { readFileSync, writeFileSync } from "fs";
 import { OpenAI } from "openai";
+import { join } from "path";
 dotenv.config();
 
+type Fruits = {
+  id: string;
+  name: string;
+  description: string;
+};
 const openai = new OpenAI();
-async function generateEmbeddings() {
+const dotProduct = (vecA: number[], vecB: number[]) => {
+  return vecA.reduce((sum, value, index) => sum + value * vecB[index], 0);
+};
+
+const cosineSimilarity = (vecA: number[], vecB: number[]) => {
+  const dotProd = dotProduct(vecA, vecB);
+  const magnitudeA = Math.sqrt(dotProduct(vecA, vecB));
+  const magniTudeB = Math.sqrt(dotProduct(vecA, vecB));
+  return dotProd / (magnitudeA * magniTudeB);
+};
+
+export function loadFruitJsonFile<T>(fileName: string): T {
+  const filePath = join(__dirname, fileName);
+  const rawData = readFileSync(filePath, "utf-8");
+  return JSON.parse(rawData.toString());
+}
+// console.log(loadFruitJsonFile("fruits.json"));
+async function generateEmbeddings(fruitDescription: string[]) {
   const response = await openai.embeddings.create({
     model: "text-embedding-3-small",
     input: "Hello, how are you ?",
   });
-  console.log(response);
+  return response;
 }
-generateEmbeddings();
+
+const saveDataToJsonFile = (fileName: string, data: any) => {
+  const filePath = join(__dirname, fileName);
+  const jsonData = JSON.stringify(data, null, 2);
+  writeFileSync(filePath, jsonData, "utf-8");
+};
+
+async function run() {
+  const fruits: Fruits[] = loadFruitJsonFile("fruits.json");
+  const fruitDescription = fruits.map((fruit) => fruit.description);
+  // console.log(fruitDescription);
+  const embeddings = await generateEmbeddings(fruitDescription);
+
+  const fruitsWithEmbeddings = fruits.map((fruit, index) => ({
+    ...fruit,
+    embedding: embeddings.data[index].embedding,
+  }));
+
+  saveDataToJsonFile("fruits_with_embeddings.json", fruitsWithEmbeddings);
+}
+run();
